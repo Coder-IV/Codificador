862dc139d33e3651fe52dce824cf34cc62983928
Luuke <137858242+Coder-IV@users.noreply.github.com>
2024-04-04 14:55:31 -0300

    Update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function resolveEquacao(a, b, x) {
 function decodificar(resultadoDesejado) {
     for (let a = -6; a <= 6; a++) {
         for (let b = -6; b <= 6; b++) {
-            let resultadoAtual = resolveEquacao(a, b, document.getElementById('valorX').value);
+            let resultadoAtual = resolveEquacao(a, b, 7);
             if (resultadoAtual === resultadoDesejado) {
                 return { a: a, b: b };
             }
@@ -14,31 +14,35 @@ function decodificar(resultadoDesejado) {
     return null;
 }
 
-document.getElementById('iniciar').addEventListener('click', function() {
+document.getElementById('iniciar').addEventListener('click', function () {
     let resultadoConhecido = parseInt(document.getElementById('valorConhecido').value);
     let palavra = document.getElementById('palavra').value.toUpperCase();
     let valoresDecodificados = decodificar(resultadoConhecido);
     let resultadoHTML = '';
 
-    if (valoresDecodificados) {
-        resultadoHTML += `<p>Para f(${document.getElementById('valorX').value}) = a${document.getElementById('valorX').value} + b = ${resultadoConhecido}, a = ${valoresDecodificados.a} e b = ${valoresDecodificados.b}</p>`
-        resultadoHTML += '<p>Valores codificados A-Z:</p>';
-        let codificado = "";
-        for (let i = 0; i < palavra.length; i++) {
-            if (palavra[i] === ' ') {
-                resultadoHTML += `<p>${palavra[i]} = Espaço</p>`;
-            } else {
-                let valorLetra = (valoresDecodificados.a * (palavra.charCodeAt(i) - 65 + 1)) + valoresDecodificados.b;
-                resultadoHTML += `<p>${palavra[i]} = ${valorLetra}</p>`;
-                codificado += `${valorLetra}-`;
-            }
-        }
-        if (codificado !== "") {
-            resultadoHTML += `<p>Criptografado: ${codificado.slice(0, -1)}</p>`;
-        }
-        resultadoHTML += `<footer>&copy; By Felipe</footer>`
+    if (valoresDecodificados.a == 0 || valoresDecodificados.b == 0) {
+        resultadoHTML = `<p>Não foi possível gerar o texto codificado, verifique se os valores fornecidos cumprem todas as regras.</p>`;
     } else {
-        resultadoHTML = `<p>Não foi possível gerar o alfabeto codificado usando engenharia reversa.</p>`;
+        if (valoresDecodificados) {
+            resultadoHTML += `<p>Para f(7) = a7 + b = ${resultadoConhecido}, a = ${valoresDecodificados.a} e b = ${valoresDecodificados.b}</p>`
+            resultadoHTML += '<p>Valor codificado de cada letra:</p>';
+            let codificado = "";
+            for (let i = 0; i < palavra.length; i++) {
+                if (palavra[i] === ' ') {
+                    resultadoHTML += `<p>${palavra[i]} = </p>`;
+                } else {
+                    let valorLetra = (valoresDecodificados.a * (palavra.charCodeAt(i) - 65 + 1)) + valoresDecodificados.b;
+                    resultadoHTML += `<p>${palavra[i]} = ${valorLetra}</p>`;
+                    codificado += `${valorLetra}-`;
+                }
+            }
+            if (codificado !== "") {
+                resultadoHTML += `<p>Criptografado: ${codificado.slice(0, -1)}</p>`;
+            }
+            resultadoHTML += `<footer>&copy; By Felipe</footer>`
+        } else {
+            resultadoHTML = `<p>Não foi possível gerar o texto codificado, verifique se os valores fornecidos cumprem todas as regras.</p>`;
+        }
     }
 
     document.getElementById('resultado').innerHTML = resultadoHTML;
